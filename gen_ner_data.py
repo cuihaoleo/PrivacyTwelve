11cd0d3c98ec9102024215e208d95b851f527d9b
CUI Hao <cuihao.leo@gmail.com>
2021-05-28 10:06:33 -0700

    update data generation code

--- gen_ner_data.py
+++ gen_ner_data.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 import random
 import re
+import tqdm
+
+import spacy
 
 
 def load_list(fname):
@@ -14,13 +18,19 @@ def load_list(fname):
 
 
 def main():
+    outfile = sys.argv[1]
+    n_samples = int(sys.argv[2])
     templates = list(load_list('template.txt'))
+
+    spacy.prefer_gpu()
+    nlp = spacy.load('en_core_web_sm')
 
     entities = []
     for ent in load_list('entities.txt'):
         ent = ent.replace('.', '')
         simp_ent = re.sub(r'\W*(Inc|Co|Corp|Corporation|Company|Ltd|Incorporated)$', '', ent, flags=re.I)
         entities.append(ent)
+
         if ent != simp_ent:
             entities.append(simp_ent)
 
@@ -34,7 +44,7 @@ def main():
 
     dataset = []
 
-    for _ in range(10000):
+    for _ in tqdm.tqdm(range(n_samples)):
         tpl = random.choice(templates)
         labels = []
 
@@ -51,9 +61,19 @@ def main():
             labels.append((m.span()[0], m.span()[0] + len(rep), m[0][1:-1]))
             tpl = tpl.replace(m[0], rep, 1)
 
+        doc = nlp(tpl)
+        for ent in doc.ents:
+            ent_label = (ent.start_char, ent.end_char, ent.label_)
+            for lb in labels:
+                if len(range(max(lb[0], ent_label[0]), min(lb[1], ent_label[1]) + 1)) > 0:
+                    break
+            else:
+                if ent_label[-1] != 'ORDINAL':
+                    labels.append(ent_label)
+
         dataset.append((tpl, {"entities": labels}))
 
-    with open("ner_dataset.json", "w") as fout:
+    with open(outfile, "w") as fout:
         json.dump(dataset, fout)
 
 
